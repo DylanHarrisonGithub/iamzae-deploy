29441c4aa5f282ab56683f570582413403475d5d
Dylan Harrison <dylanharrison@Dylans-Mac-mini.local>
2023-11-09 17:11:29 -0600

    events single event view encoded in url from derived event

--- server/routes/eventstream/eventstream.route.js
+++ server/routes/eventstream/eventstream.route.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -73,7 +84,7 @@ exports.default = (request) => __awaiter(void 0, void 0, void 0, function* () {
                 messages: [
                     `SERVER - ROUTES - EVENTSTREAM - Events streamed.`
                 ].concat(dbRes.messages),
-                body: events
+                body: events.map(e => { const { search } = e, filteredE = __rest(e, ["search"]); return filteredE; })
             }
         }));
     }
