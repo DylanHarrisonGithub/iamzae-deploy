554f532ec2062e0ba134f6111469333b4815ef5c
Dylan Harrison <dylanharrison@Macmini.attlocal.net>
2025-07-15 19:59:46 -0500

    major bug squashed! storage service storage object was sometimes unresolved promise

--- server/config/config.js
+++ server/config/config.js
@@ -2,16 +2,17 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const crypto_1 = __importDefault(require("crypto"));
 const secret = crypto_1.default.randomBytes(64).toString('hex');
+const env = ((_a = process.env.NODE_ENV) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'DEVELOPMENT';
 const config = {
-    SERVER_SECRET: process.env.IAMZAE_SERVER_SECRET || secret,
+    SERVER_SECRET: env === 'DEVELOPMENT' ? 'abcdefg' : process.env.IAMZAE_SERVER_SECRET || secret,
     DATABASE_URL: process.env.IAMZAE_DATABASE_URL || '',
     APPNAME: 'iamzae',
-    ENVIRONMENT: ((_a = process.env.NODE_ENV) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'DEVELOPMENT',
+    ENVIRONMENT: ((_b = process.env.NODE_ENV) === null || _b === void 0 ? void 0 : _b.toUpperCase()) || 'DEVELOPMENT',
     SOCKET_CONNECT_PRIVELEGE: ['guest', 'user', 'admin'],
     ROOT_DIR: path_1.default.normalize(__dirname + `/../../`),
     PORT: process.env.IAMZAE_PORT || 3000,
